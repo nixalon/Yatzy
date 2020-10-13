f035ec628316b49a22480c33a0a8d663a2f6a4f3
nixalon <47711662+nixalon@users.noreply.github.com>
2020-10-13 18:18:40 +0200

    Update YatziTest.java

--- src/kyh/labs/lab5/YatziTest.java
+++ src/kyh/labs/lab5/YatziTest.java
@@ -32,27 +32,3 @@ public class YatziTest {
     }
 
 }
-
-
-/*
-public class YatziTest {
-    @Test
-    void isYatziWhenAllDiceMatches() {
-        Die[] dice = new Die[5];
-        for(Die die: dice) {
-            die.value = 6;
-        }
-        //Assert something?
-    }
-
-    @Test
-    void isNotYatziWhenOneDieIsNotMatchingTheOther() {
-        Die[] dice = new Die[5];
-        for(Die die: dice) {
-            die.value = 6;
-        }
-        dice[5].value = 1;
-        //Assert something?
-    }
-}
-*/
