6942281e8c3937aea8fc771049d119f4868d65de
nixalon <47711662+nixalon@users.noreply.github.com>
2020-10-13 18:19:17 +0200

    Update YatziMain.java
    deleted comments

--- src/kyh/labs/lab5/YatziMain.java
+++ src/kyh/labs/lab5/YatziMain.java
@@ -39,7 +39,7 @@ public class YatziMain {
     }
 
     private static boolean rollDiceAndCheckIfYatzi(){
-        Die[] dice = new Die[5];                                   //
+        Die[] dice = new Die[5];                                   
         for (int i = 0; i < 5; i++) {
             dice [i] = new Die();
         }
@@ -59,62 +59,4 @@ public class YatziMain {
             }
         return yatzi;                                               // Returnera yatsi-flaggan.
         }
-
-
-/*  Den gamla koden, för referens.
-
-        int iturn = 0;
-        ds = new Die[5];
-        for(int d=0;d<5;d++) {
-            ds[d] = new Die();
-        }
-        //We will continue until the game is over
-
-
-        while(bGameIsOn == true) {
-            iturn = 0;
-            System.out.println("Welcome to Yatzi!");
-            while(iturn < 3) {
-                System.out.println("Starting turn " + (iturn+1) + " of 3, rolling dice.");
-                for(int i=0;i<ds.length;i++) {
-                    ds[i].DieRoll();
-                    //ds[i].value = 5; //Test if yatzi work
-                    System.out.println(i + ": " + ds[i].getString());
-                }
-                //YATZI
-                boolean flag = true;
-                for(int j=1;j<5;j++) {
-                    if(ds[j].value!=ds[j-1].value) {
-                        //Set flag to false
-                        flag = false;
-                    }
-                }
-                if(flag == true) {
-                    System.out.println("You got YATZI! in " + ds[0].value + "'s");
-                    return;
-                } else {
-                    //Here we check if there is no Yatzy: then we check what turn we are on and asks the player if we want to continue or not
-                    if(iturn != 2) {
-                        System.out.println("Want to throw again? (y for yes, anything else for no)");
-                        Scanner sc = new Scanner(System.in);
-                        if(sc.next().equals("y")) {
-                            ++iturn;
-                        } else {
-                            bGameIsOn = !bGameIsOn;
-                            break;
-                        }
-                    } else {
-                        System.out.println("Game over! Want to play again?");
-                        Scanner sc = new Scanner(System.in);
-                        if(sc.next().equals("y")) {
-                            iturn = 0;
-                        } else {
-                            bGameIsOn = !bGameIsOn;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }*/
 }
